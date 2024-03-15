4188979d72418d0594e963c03e125fa3fde7f1c7
Daniel <danielmolina81@hotmail.com>
2024-03-14 21:31:59 -0400

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,86 +1,64 @@
-
-#Import Modules needed
+# Import Modules needed
 import os
 import csv
 
-#Set the path for the csv file
-csvpath = os.path.join("PyBank/Resources/budget_data.csv")
+# Set the path for the csv file
+budgetpath = os.path.join("PyBank/Resources/budget_data.csv")
 
-#Open the CSV 
-with open(csvpath) as budgetdata:
-    budgetreader = csv.reader(budgetdata, delimiter=',')
+# Open the CSV 
+with open(budgetpath,'r') as budgetdata:
+    budgetreader = csv.reader(budgetdata)
     
-    #Extract header
+    # Extract header
     header = next(budgetreader)
     
+    # Define Lists and Variables
     ProfitLosses = []
+    months = []
     totalPL = 0
     changes = []
     change = 0
     totalchanges=0
 
-       # Loop through data
+    # Loop through data to obtain a list of profits and losses and a list of months
     for month in budgetreader:
         ProfitLosses.append(int(month[1]))
+        months.append((month[0]))
 
+    # Loop through the list of profits and losses to obtain the total Profits/Losses
     for month in ProfitLosses:
         totalPL += month
 
+    # Loop through the list of profits and losses to calculate the changes month by month
     for month in range(1,len(ProfitLosses)):
         change = ProfitLosses[month] - ProfitLosses[month-1]
-        changes.append(int(change))
 
+        # Create a list with the changes
+        changes.append(int(change))
+    
+    # Loop through the list of changes to calculate the total changes
     for change in changes:
         totalchanges += change
     
+    # Calculate the average of the changes
     averagechanges = totalchanges/len(changes)
 
-print(
-f"Financial Analysis\n\n\
-----------------------------\n\n\
-Total Months: {len(ProfitLosses)}\n\n\
-Total: $ {totalPL}\n\n\
-Average Change: $ {round(averagechanges,2)}\n\n\
-Greatest Increase in Profits: Aug-16 ($ {max(changes)})\n\n\
-Greatest Decrease in Profits: Feb-14 ($ {min(changes)})\n")
-    
-#Dim Total_Rows As Double
-#Dim Total_Months As Double
-#Dim Total As Double
-#Dim Change As Double
-#Dim Greatest_Increase_Month As Double
-#Dim Greatest_Increase As Double
-#Dim Greatest_Decrease_Month As Double
-#Dim Greatest_Decrease As Double
-#Dim Total_Change As Double
-#Dim Average_Change As Double
+# Print the analysis header on Terminal
+print(f"Financial Analysis\n\n\----------------------------\n")
 
-# Total_Rows = Cells(Rows.Count, 1).End(xlUp).Row
-# Total_Months = Total_Rows-1
-# Total = cells(2,2).value
+# Print Total months          
+print(f"Total Months: {len(ProfitLosses)}\n")
 
-# for i = 3 to Total_Rows
-#     Total = Total + Cells(i,2).value
-#     Change = Cells(i,2).value-Cells(i-1,2).value
-#         If Change>Greatest_Increase then
-#             Greatest_Increase = Change
-#             Greatest_Increase_Month = Cells(i,1).value
-#         End if
-#         If Change<Greatest_Decrease then
-#             Greatest_Decrease = Change
-#             Greatest_Decrease_Month = Cells(i,1).value
-#         End if
-#     Total_Change = Total_Change + Change
+# Print Total Profits/Losses
+print(f"Total: $ {totalPL}\n")
 
-# Next i
+# Print Average changes
+print(f"Average Change: $ {round(averagechanges,2)}\n")
 
-# Average_Change = Total_Change/(Total_Months-1)
+# Print Greatest Increase
+print(f"Greatest Increase in Profits: Aug-16 ($ {max(changes)})\n")
 
-# Cells (2,5).value = Total_Months
-# Cells(3,5).value= Total
-# Cells(4,5).value = Average_Change
-# Cells(5,4).value = Greatest_Increase_Month
-# Cells(5,5).value = Greatest_Increase
-# Cells(6,4).value = Greatest_Decrease_Month
-# Cells(6,5).value = Greatest_Decrease
-    
+# Print Greatest Decrease
+print(f"Greatest Decrease in Profits: Feb-14 ($ {min(changes)})\n")
+
+analysispath = os.path.join("PyBank/Analysis/Analysis.txt")
